c9d71394210a758f6b80a8be9005c50f8b17d2e3
jerome.papalie <jerome.papalie@gmail.com>
2024-02-29 16:55:20 +0800

    added v2 endpoint added logic to reverse the string and encode to md5

--- src/main/java/com/beta/replyservice/ReplyController.java
+++ src/main/java/com/beta/replyservice/ReplyController.java
@@ -7,13 +7,53 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class ReplyController {
 
-	@GetMapping("/reply")
-	public ReplyMessage replying() {
-		return new ReplyMessage("Message is empty");
-	}
+    @GetMapping("/reply")
+    public ReplyMessage replying() {
+        return new ReplyMessage("Message is empty");
+    }
 
-	@GetMapping("/reply/{message}")
-	public ReplyMessage replying(@PathVariable String message) {
-		return new ReplyMessage(message);
-	}
+    @GetMapping("/reply/{message}")
+    public ReplyMessage replying(@PathVariable String message) {
+        return new ReplyMessage(message);
+    }
+
+    // new functionality at v2 endpoint
+    @GetMapping("/v2/reply/{message}")
+    public ReplyMessage replyingV2(@PathVariable String message) {
+
+        // String Parsing logic
+        String[] splitString = message.split("-", 2);
+        String ruleString = splitString[0];
+        String messageString = splitString[1];
+        String processedString = messageString;
+
+        // Check if message is empty or if the format is violated for this endpoint eg:
+        if (splitString.length < 2) {
+            return new ReplyMessage("Invalid input format. Expected format: {rule}-{string}");
+        } else if (splitString[1].isEmpty()) {
+            return new ReplyMessage("Message is empty");
+        }
+
+        // Iterate through array of rules
+        for (char rules : ruleString.toCharArray()) {
+            processedString = rulesSwitchCase(processedString, rules);
+        }
+        
+        return new ReplyMessage(processedString);
+    }
+    
+    // Rules selection function
+    private String rulesSwitchCase(String input, char rule) {
+        switch (rule) {
+            case '1':
+                input = ReplyMessage.reverseString(input);
+                return input;
+            case '2':
+                input = ReplyMessage.encodeStringtoMD5(input);
+                return input;
+            default:
+                return "Invalid Input";
+        }
+    }
+
 }
--- src/main/java/com/beta/replyservice/ReplyMessage.java
+++ src/main/java/com/beta/replyservice/ReplyMessage.java
@@ -1,14 +1,42 @@
 package com.beta.replyservice;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class ReplyMessage {
 
-	private final String message;
+    private final String message;
 
-	public ReplyMessage(String message) {
-		this.message = message;
-	}
+    public ReplyMessage(String message) {
+        this.message = message;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
+
+    public static String encodeStringtoMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes());
+
+            // Convert the byte array to a Hex String
+            StringBuilder hexString = new StringBuilder();
+            
+            for (byte b : digest) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return "Error encoding the string";
+        }
+    }
+
+    public static String reverseString(String input) {
+        String reversedString = new StringBuilder(input).reverse().toString();
+        return reversedString;
+    }
 }
